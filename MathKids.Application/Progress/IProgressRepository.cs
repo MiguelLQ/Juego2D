ad6da071cf78721c169a78f8eab0c6a00d80486c
alex1609antnohy <1006020202@unajma.edu.pe>
2026-07-16 23:01:51 -0500

    Alex Comit

--- MathKids.Application/Progress/IProgressRepository.cs
+++ MathKids.Application/Progress/IProgressRepository.cs
@@ -3,3 +3,5 @@ using MathKids.Domain.Progress;
 namespace MathKids.Application.Progress;
 
 public interface IProgressRepository { PlayerProgress Get(Guid profileId); void Save(PlayerProgress progress); }
+
+//hola mundo
